a48c4653e60dcd1472dee76b3739e12dbc7481b2
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-10-27 13:12:16 -0600

    added ssl certificate

--- aws-static-website/main.tf
+++ aws-static-website/main.tf
@@ -1,7 +1,53 @@
+locals {
+  full_domain_name = var.subdomain != "" ? "${var.subdomain}.${var.root_domain}" : "${var.root_domain}"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
+resource "aws_acm_certificate" "main" {
+  domain_name       = local.full_domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/route53_zone
+data "aws_route53_zone" "main" {
+  name = var.root_domain
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
+resource "aws_route53_record" "certificate_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main.zone_id
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn = aws_acm_certificate.main.arn
+
+  timeouts {
+    create = "1m"
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "main" {
-  # Bucket name for static website hosting must be the domain name (including subdomain and top level domain)
-  bucket = var.subdomain != "" ? "${var.subdomain}.${var.root_domain}" : "${var.root_domain}"
+  # Bucket name for static website hosting must be the full domain name (subdomain, domain name and top-level domain)
+  bucket = local.full_domain_name
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
--- aws-static-website/versions.tf
+++ aws-static-website/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.2.9"
+  required_version = "~>1.3.0"
 
   # backend "s3" {
   #   bucket         = "depploy-terraform-state-production"
